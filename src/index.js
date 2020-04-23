d4196f39e2d95600b9a93a391134b9d3ea1f44c7
Denys Morozov <denysbedbug@gmail.com>
2020-04-23 18:03:21 +0300

    fix: removed footer spacer at all

--- src/index.js
+++ src/index.js
@@ -145,8 +145,7 @@ class ParallaxScrollView extends Component {
 						// onScroll: this._onScroll.bind(this)
 					},
 					foreground,
-					bodyComponent,
-					footerSpacer
+					bodyComponent
 				)}
 				{maybeStickyHeader}
 			</View>
